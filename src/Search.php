9d2033a868cae2b9f6ee35ec9a6e28e8e579dc44
Leonardo Neves <69824231+LeooNevees@users.noreply.github.com>
2021-08-03 20:40:10 -0300

    Incrementado timeout

--- src/Search.php
+++ src/Search.php
@@ -1,14 +1,34 @@
 <?php
+
 namespace Wead\DigitalCep;
+
+use Exception;
 
 class Search{
     private $url = "https://viacep.com.br/ws/";
 
     public function getAddressFromZipCode(string $zipCode){
-        $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
+        try {
+            ini_set('default_socket_timeout', 10);
 
-        $get = file_get_contents($this->url . $zipCode . "/json");
-        
-        return (array) json_decode($get);
+            $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
+
+            $get = file_get_contents($this->url . $zipCode . "/json");
+            if($get === false){
+                throw new Exception('Erro ao tentar estabelecer conexão com o site Viacep. Por favor tente novamente mais tarde (TIMEOUT)');
+            }
+
+            $retornoDecod = count((array)json_decode($get)) ? (array)json_decode($get) : 'Nenhum registro encontrado com esse CEP';
+            
+            return array(
+                'status' => 'Retorno obtido com suceso',
+                'mensagem' => $retornoDecod
+            );
+        } catch (Exception $ex) {
+            return array(
+                'status' => 'ERRO',
+                'mensagem' => $ex->getMessage()
+            );
+        }
     }
 }
